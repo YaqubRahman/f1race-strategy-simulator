7444d7d4c346eca02c4aa1d1205843c957189641
Yaqub Rahman <yaqubrahman999@gmail.com>
2025-12-04 16:53:37 +0000

    feat: pitstop laps shown in red in graph

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 
 print(fastf1.__version__)
 fastf1.Cache.enable_cache('cache')  
-session = fastf1.get_session(2023, 'Abu Dhabi', 'R') 
+session = fastf1.get_session(2024, 'Abu Dhabi', 'R') 
 session.load()
 
 
@@ -18,11 +18,17 @@ laps["LapTimeSeconds"] = laps["LapTime"].dt.total_seconds()
 # Lap times in seconds
 laps['LapTimeSeconds'] = laps['LapTime'].dt.total_seconds()
 
+# Marking the pit stop laps/spikes in the graph
+pit_laps = laps[laps['PitOutTime'].notna()]
+
 plt.figure(figsize=(10,6))
 plt.plot(laps['LapNumber'], laps['LapTimeSeconds'], marker='o')
 plt.xlabel("Lap Number")
 plt.ylabel("Lap Time (s)")
-plt.title("Verstappen Lap Times - Abu Dhabi 2023")
+plt.title("Verstappen Lap Times - Abu Dhabi 2024")
+plt.grid(which='both', linestyle='--', linewidth=0.5)
+for lap in pit_laps['LapNumber']:
+    plt.axvline(x=lap, color='r', linestyle='--', alpha=0.5)
 plt.show()
 
 
