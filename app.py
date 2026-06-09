62ce38e9f2d8fa9f275840ede044bd8dad316efd
YaqubRahman <YaqubRahman999@gmail.com>
2026-06-09 22:02:37 +0100

    feat: routing in app.py for flask

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from main import predict_strategy
+from main import train_model
 
 app = Flask(__name__)
+CORS(app)
 
-# @app.route('/')
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    data = request.get_json()
+    driver = data['driver']
+    circuit = data['circuit']
+
+    reg, feature_columns, multi_year_laps, session, avg_delta_soft, avg_delta_medium, avg_delta_hard = train_model(driver, circuit, [2018, 2019, 2020, 2021, 2022, 2023, 2024])
+    best_strategy, best_compound1, best_compound2, best_time = predict_strategy(reg, feature_columns, multi_year_laps, session, avg_delta_soft, avg_delta_medium, avg_delta_hard)
+    
+    return jsonify({
+        "best_strategy": best_strategy,
+        "best_compound1": best_compound1,
+        "best_compound2": best_compound2,
+        "best_time": best_time
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight text-center mt-10">
+      <h2 className=" font-mono text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight text-center mt-10">
         F1 Pitstop Predictor
       </h2>
     </>
